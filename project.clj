e750b0a358ecf213bf3dd2326da1dbe568d1e263
Moe Aboulkheir <moe@nervous.io>
2019-02-01 01:24:08 +0000

    Initial implementation. (#1)

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject io.datopia/codox-theme "0.1.0"
+  :description "Datopia Codox theme"
+  :license     {:name "MIT License"
+                :url  "http://opensource.org/licenses/MIT"}
+  :scm         {:name "git"
+                :url  "https://github.com/datopia/codox-theme"}
+  :url         "https://github.com/datopia/codox-theme")
